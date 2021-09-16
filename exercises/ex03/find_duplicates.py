22f4d3cb74e9b3125345a3834b4ca0647975ecda
Sara Kent Trolllinger <sarakent@email.unc.edu>
2021-09-15 20:57:14 -0400

    ex03 work

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,21 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730393060"
+word: str = input("Enter a word: ")
+
+i: int = 0
+
+dup: bool = False
+
+while i < len(word):
+    char = word[i]
+    j: int = i + 1
+    while j < len(word):
+        if char == word[j]:
+            dup = True
+            j += 1
+        else:
+            j += 1
+    i += 1
+
+print("Found duplicate: " + str(dup))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,18 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730393060"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+depth: int = int(input("Depth: "))
+counter: int = 0
+sequence = TREE + " "
+final_sequence = TREE
+
+if depth > 0:
+    if counter == depth:
+        print(sequence * counter + final_sequence)
+    while counter < depth:
+        print(sequence * counter + TREE)
+        counter += 1
