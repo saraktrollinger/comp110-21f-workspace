160719805873235671f94db3d013da359d3505d3
Sara Kent Trolllinger <sarakent@email.unc.edu>
2021-10-06 14:06:38 -0400

    ex05 progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,48 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730393060"
 
 # Define your functions below
+
+
+def only_evens(list_one: list[int]) -> list[int]:
+    """Return a list of only the even numbers."""
+    i: int = 0
+    list_two: list[int] = list()
+    while i <= len(list_one):
+        if list_one[i] % 2 == 0:
+            list_two.append(i)
+        i += 1
+    return list_two
+
+
+def sub(initial: list[int], start: int, end: int) -> list[int]:
+    """Return the initial list of start index until end index minus one."""
+    i: int = 0 + start
+    new_end: int = end - 1
+    new_list: list[int] = list()
+    while i <= new_end:
+        if i == len(initial):
+            return new_list
+        else:
+            if i < 0:
+                i = initial[0]
+                new_list.append(i)
+            else:
+                new_list.append(i)
+            i += 1
+    return new_list
+
+
+def concat(list_one: list[int], list_two: list[int]) -> list[int]:
+    """Returns a list of both inputed lists."""
+    new_str: list[int] = list()
+    i: int = 0
+    while i <= len(list_one):
+        new_str.append(i)
+        i += 1
+    i: int = 0
+    while i <= len(list_two):
+        new_str.append(i)
+        i += 1
+    return new_str
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,60 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730393060"
+
+
+def test_only_evens_empty() -> None:
+    list_one: list[int] = []
+    assert only_evens(list_one) == []
+
+
+def test_only_evens_single_item() -> None:
+    list_one: list[int] = [4]
+    assert only_evens(list_one) == [4]
+
+
+def test_only_evens_many_items() -> None:
+    list_one: list[int] = [3, 8, 7, 2]
+    assert only_evens(list_one) == [8, 2]
+
+
+def test_sub_empty() -> None:
+    initial: list[int] = []
+    start: int = 0
+    end: int = 0
+    assert sub(initial, start, end) == []
+
+
+def test_sub_single_item() -> None:
+    initial: list[int] = [4, 5, 6, 7]
+    start: int = 4
+    end: int = 5
+    assert sub(initial, start, end) == [4]
+
+
+def test_sub_many_items() -> None:
+    initial: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    start: int = 3
+    end: int = 11
+    assert sub(initial, start, end) == [3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_concat_empty() -> None:
+    list_one: list[int] = []
+    list_two: list[int] = []
+    assert concat(list_one, list_two) == []
+
+
+def test_concat_single_item() -> None:
+    list_one: list[int] = [1]
+    list_two: list[int] = [4]
+    assert concat(list_one, list_two) == [1, 4]
+
+
+def test_concat_many_items() -> None:
+    list_one: list[int] = [4, 6, 19]
+    list_two: list[int] = [6, 7, 3]
+    assert concat(list_one, list_two) == [4, 6, 19, 6, 7, 3]
