647edcce59fd0893b529c41c106b1e831361f7c8
Sara Kent Trolllinger <sarakent@email.unc.edu>
2021-10-06 21:16:10 -0400

    ex05 work

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,14 +2,12 @@
 
 __author__ = "730393060"
 
-# Define your functions below
-
 
 def only_evens(list_one: list[int]) -> list[int]:
     """Return a list of only the even numbers."""
     i: int = 0
     list_two: list[int] = list()
-    while i <= len(list_one):
+    while i <= len(list_one) - 1:
         if list_one[i] % 2 == 0:
             list_two.append(i)
         i += 1
@@ -21,7 +19,7 @@ def sub(initial: list[int], start: int, end: int) -> list[int]:
     i: int = 0 + start
     new_end: int = end - 1
     new_list: list[int] = list()
-    while i <= new_end:
+    while i <= new_end - 1:
         if i == len(initial):
             return new_list
         else:
@@ -38,11 +36,11 @@ def concat(list_one: list[int], list_two: list[int]) -> list[int]:
     """Returns a list of both inputed lists."""
     new_str: list[int] = list()
     i: int = 0
-    while i <= len(list_one):
+    while i <= len(list_one) - 1:
         new_str.append(i)
         i += 1
     i: int = 0
-    while i <= len(list_two):
+    while i <= len(list_two) - 1:
         new_str.append(i)
         i += 1
     return new_str
