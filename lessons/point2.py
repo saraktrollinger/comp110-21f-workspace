1b656643acfffa062d8ee1a93870ef1305584fee
Sara Kent Trolllinger <sarakent@email.unc.edu>
2021-11-12 23:50:55 -0500

    progress

---
+++ lessons/point2.py
@@ -0,0 +1,46 @@
+"""Example of a point class part 2."""
+
+from __future__ import annotations
+
+
+class Point:
+    x: float
+    y: float
+
+    def __init__(self, x: float, y: float):
+        """Construct a point by giving x and y args."""
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        """Special method to represent object as string."""
+        return f"{self.x}, {self.y}"
+
+    def __mul__(self, factor: float) -> Point:
+        """Overload the multiplication operator for Point * float."""
+        print("__mul__ was called.")
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, rhs: Point) -> Point:
+        print("__add__ was called.")
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+    def __getitem__(self, index: int) -> float:
+        """Overload the subcription notation."""
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.x
+        else:
+            raise IndexError
+
+
+a: Point = Point(1.0, 2.0)
+b: Point = a * 2.0
+c: Point = a + b
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"bc: {c}")
+
+print(a[0])
+print(a[1])
---
+++ projects/pj01/data_utils.py
@@ -0,0 +1,96 @@
+"""Utility functions."""
+
+__author__ = "730393060"
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of the csv into a 'table'."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(data_rows: list[dict[str, str]]) -> dict[str, list[str]]:
+    """List of rows turned into dictionary of columns."""
+    data_cols: dict[str, list[str]] = {}
+    first_row: dict[str, str] = data_rows[0]
+    for column in first_row:
+        data_cols[column] = column_values(data_rows, column)
+    return data_cols
+
+
+def head(column_based: dict[str, list[str]], rows_included: int) -> dict[str, list[str]]:
+    """Produce a new column-based table with only the first row of data."""
+    final: dict[str, list[str]] = {}
+    for column in column_based:
+        if rows_included >= len(column_based[column]):
+            return column_based
+        else:
+            new: list[str] = []
+            point = column_based[column]
+            i: int = 0
+            while i < rows_included:
+                new.append(point[i])
+                i += 1
+            final[column] = new
+    return final
+
+
+def select(column_based: dict[str, list[str]], names: list[str]) -> dict[str, list[str]]:
+    """Produce a new column-based table with only a subset of original columns."""
+    returned: dict[str, list[str]] = {}
+    for n in names:
+        returned[n] = column_based[n]
+    return returned
+
+
+def concat(column_one: dict[str, list[str]], column_two: dict[str, list[str]]) -> dict[str, list[str]]:
+    """New dictionary from two combined column-based tables."""
+    ending: dict[str, list[str]] = {}
+    for column in column_one:
+        ending[column] = column_one[column]
+    for column in ending:
+        if column in ending:
+            column_one[column] + column_two[column]
+        else:
+            ending[column] = column_two[column]
+    return ending
+
+
+def count(frequencies: list[str]) -> dict[str, int]:
+    """Count the total of all students in each major."""
+    result: dict[str, int] = {}
+    for key in frequencies:
+        if key in result:
+            result[key] += 1
+        else:
+            result[key] = 1
+    return result
+
+
+def count_again(frequencies: list[str]) -> dict[str, int]:
+    """Count all non-comp majors."""
+    result: dict[str, int] = {}
+    for key in frequencies:
+        if key == "Computer Science":
+            key == "Computer Science"
+            result["Computer Science"] += 1
+        else: 
+            key == "Other"
+            result["Other"] += 1
+    return result
