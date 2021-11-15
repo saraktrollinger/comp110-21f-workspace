25293fb545d810614fa29c239c5f3e47e4be5fc2
Sara Kent Trolllinger <sarakent@email.unc.edu>
2021-11-15 16:45:48 -0500

    updates

---
+++ lessons/special_params.py
@@ -0,0 +1,26 @@
+"""Examples of optional parameters and union types."""
+
+from typing import Union
+
+
+def hello(name: Union[str, int, float] = "World") -> str:
+    """A delightful greeting."""
+    greeting: str = "Hello, "
+    if isinstance(name, str):
+        greeting += "Hello, " + name
+    elif isinstance(name, int):
+        greeting += "COMP" + str(name)
+    else:
+        greeting += "Alien Life from Sector" + str(name)
+    return greeting
+
+
+# Single-argument
+print(hello("Sally"))
+
+# No arguments
+print(hello())
+
+# int Argument works too:
+print(hello(110))
+print(hello(3.14))
